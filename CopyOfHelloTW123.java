006244cebf1ee5ea343bcd24375de0f5cd314b36
axlrock1021 <axlrock1021@hotmail.com>
2019-01-11 17:08:14 +0800

    fffff

---
+++ CopyOfHelloTW123.java
@@ -0,0 +1,14 @@
+package tw.com.axlrock1021;
+/**
+ * 列印出"我愛台灣!"
+ * @author Andy
+ * @version 1.0
+ * @date 2019年1月11日 下午3:31:52
+ */
+public class CopyOfHelloTW123 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("我愛台灣!"); //印出字串訊息
+	}
+}
