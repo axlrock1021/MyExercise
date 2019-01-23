915e585862f1ece2e7ce0cbbd9bd1aafd30969e8
axlrock1021 <axlrock1021@hotmail.com>
2019-01-23 14:01:57 +0800

    Math API

---
+++ MathDemo_34.java
@@ -0,0 +1,18 @@
+package tw.com.axlrock1021;
+/**
+ * Math API
+ * @author Andy
+ * @version 1.0
+ * @date 2019年1月23日 下午1:59:55
+ */
+public class MathDemo_34 {
+
+	public static void main(String[] args) {
+	
+		System.out.println(Math.ceil(20.5)); //(天花板)進一 
+		System.out.println(Math.floor(20.5)); //(地板)去尾
+		
+		System.out.println(Math.ceil(-20.5)); //去尾
+		System.out.println(Math.floor(-20.5)); //進一
+	}
+}
