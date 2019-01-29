c50b77defe33cb64eeaf193c0bdcddeb829babd8
axlrock1021 <axlrock1021@hotmail.com>
2019-01-29 10:43:29 +0800

    猜拳遊戲主程式

---
+++ guess/GuessMainDemo_41.java
@@ -0,0 +1,14 @@
+package tw.com.axlrock1021.guess;
+/**
+ * 猜拳遊戲主程式
+ * @author Andy
+ * @version 1.0
+ * @date 2019年1月28日 下午12:09:47
+ */
+public class GuessMainDemo_41 {
+
+	public static void main(String[] args) {
+		
+		new GameRoomDemo_41("admin","123");		
+	}
+}
